bdcfc196c07cdf495016034c8bc5aee3602666a1
koakh <kklweb28>
2017-11-05 01:34:13 +0000

    Reorganize files structure

--- src/main/java/com/koakh/springbootgraphqlstarter/dao/AuthorDao.java
+++ src/main/java/com/koakh/springbootgraphqlstarter/dao/AuthorDao.java
@@ -9,10 +9,12 @@ public class AuthorDao {
   private List<Author> authors;
 
   public AuthorDao(List<Author> authors) {
+
     this.authors = authors;
   }
 
   public Optional<Author> getAuthor(String id) {
+
     return authors.stream()
       .filter(author -> id.equals(author.getId()))
       .findFirst();
---
+++ src/main/java/com/koakh/springbootgraphqlstarter/repository/zs.java
@@ -0,0 +1,4 @@
+package com.koakh.springbootgraphqlstarter.repository;
+
+public class zs {
+}
